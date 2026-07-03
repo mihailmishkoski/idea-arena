4fce0a9c76000483aa4dde6f5beb143fbb859a2a
Mihail <miskoski555@gmail.com>
2026-07-03 23:41:11 +0200

    Upgrade Angular 17 -> 18 (provideHttpClient migration)

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CredentialsInterceptor } from './interceptors/credentials.interceptor';
 
@@ -6,12 +6,10 @@ import { CredentialsInterceptor } from './interceptors/credentials.interceptor';
  * Singleton services and app-wide providers. Imported exactly once by the
  * AppModule; the guard below throws if a lazy module pulls it in again.
  */
-@NgModule({
-  imports: [HttpClientModule],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: CredentialsInterceptor, multi: true },
-  ],
-})
+@NgModule({ imports: [], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: CredentialsInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parent: CoreModule) {
     if (parent) {
